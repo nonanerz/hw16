18030733c7f8b5c0dbb6a3e20b41b85b35b0607c
Bulava Eduard <bulavaeduard@gmail.com>
2018-03-03 17:34:03 +0200

    create event

--- www/js/index.js
+++ www/js/index.js
@@ -4,7 +4,7 @@
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
+ * "License") you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -19,7 +19,7 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false)
     },
 
     // deviceready Event Handler
@@ -27,73 +27,98 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        this.receivedEvent('deviceready')
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        var parentElement = document.getElementById(id)
+        var listeningElement = parentElement.querySelector('.listening')
+        var receivedElement = parentElement.querySelector('.received')
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        listeningElement.setAttribute('style', 'display:none')
+        receivedElement.setAttribute('style', 'display:block')
 
-        console.log('Received Event: ' + id);
+        console.log('Received Event: ' + id)
     }
-};
+}
 
 function initMap() {
-    var myLatlng = {lat: 49.445, lng: 32.061};
+    var myLatlng = {lat: 49.445, lng: 32.061}
 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8,
         center: myLatlng
-    });
+    })
     google.maps.event.addListener(map, 'click', function(event) {
-        $('#form').show();
-        $('#lat').val(event.latLng.lat());
-        $('#lng').val(event.latLng.lng());
-    });
+        $('#form').show()
+        $('#lat').val(event.latLng.lat())
+        $('#lng').val(event.latLng.lng())
+    })
 
 }
 $(document).ready(function () {
-  $("#submit").click(function () {
-    var title = $("#title").val();
-    var description = $("#description").val();
-    var selectedDate = $('#datepicker').val();
+    $("#submit").click(function () {
+    var title = $("#title").val()
+    var description = $("#description").val()
+    var selectedDate = $('#datetimepicker').data("DateTimePicker").date()
+    var lng = $('#lng').val()
+    var lat = $('#lat').val()
 
-    var isValid = true;
+    var isValid = true
 
     if (title === "") {
-      $("#title_error").text("title is required");
-      isValid = false;
+      $("#title_error").text("title is required")
+      isValid = false
     } else {
-      $("#title").text("");
+      $("#title").text("")
     }
 
     if (description === "") {
-      $("#description_error").text("description is required");
-      isValid = false;
+      $("#description_error").text("description is required")
+      isValid = false
     } else {
-      $("#description").text("");
+      $("#description").text("")
     }
 
     if (selectedDate === "") {
-      $("#datepicker_error").text("date is is required");
-      isValid = false;
+      $("#datepicker_error").text("date is is required")
+      isValid = false
     } else {
-      $("#datepicker").text("");
+      $("#datepicker").text("")
     }
 
     if (isValid) {
-      $("#text_form").submit();
+        selectedDate = moment(selectedDate).format()
+
+
+        $.ajax({
+            url: 'https://polar-gorge-30507.herokuapp.com/api/v1/events',
+            type: 'post',
+            data: JSON.stringify({
+                title: title,
+                description: description,
+                date: selectedDate,
+                lng: lng,
+                lat: lat
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            dataType: 'json',
+            success: function (data) {
+                $('#form').hide()
+                $("#title").val('')
+                $("#description").val('')
+                $('#datepicker').val('')
+            }
+        });
     }
-  });
+  })
 
-  $("title").focus();
-});
+  $("title").focus()
+})
 
 
 
-app.initialize();
+app.initialize()
